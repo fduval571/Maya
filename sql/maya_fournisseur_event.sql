4c16907d68abbc3758e5148f00a8b276c1a197c9
Byakuya98 <byakuya98@hotmail.fr>
2020-03-30 17:26:03 +0200

    Fournisseur et Evenement

---
+++ sql/maya_fournisseur_event.sql
@@ -0,0 +1,239 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  lun. 30 mars 2020 à 15:23
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `maya`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `animal`
+--
+
+DROP TABLE IF EXISTS `animal`;
+CREATE TABLE IF NOT EXISTS `animal` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `race_id` int(11) NOT NULL,
+  `nom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_naissance` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_6AAB231F6E59D40D` (`race_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `animal`
+--
+
+INSERT INTO `animal` (`id`, `race_id`, `nom`, `date_naissance`) VALUES
+(3, 2, 'chat', '1992-02-10');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`id`, `libelle`) VALUES
+(77, 'Fruits'),
+(79, 'Test'),
+(80, 'Test');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `evenement`
+--
+
+DROP TABLE IF EXISTS `evenement`;
+CREATE TABLE IF NOT EXISTS `evenement` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` date NOT NULL,
+  `horaire` time NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `evenement`
+--
+
+INSERT INTO `evenement` (`id`, `titre`, `description`, `date`, `horaire`) VALUES
+(3, 'test', 'c\'est un test', '2015-01-01', '00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fournisseur`
+--
+
+DROP TABLE IF EXISTS `fournisseur`;
+CREATE TABLE IF NOT EXISTS `fournisseur` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `adresse` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mail` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telephone` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nom_contact` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_relation` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `fournisseur`
+--
+
+INSERT INTO `fournisseur` (`id`, `nom`, `adresse`, `mail`, `telephone`, `nom_contact`, `date_relation`) VALUES
+(2, 'Jean', 'Ars-Sur-Moselle', 'jean@gmail.com', '0698829815', 'Mensoif', '2022-01-01');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `migration_versions`
+--
+
+DROP TABLE IF EXISTS `migration_versions`;
+CREATE TABLE IF NOT EXISTS `migration_versions` (
+  `version` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `executed_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `migration_versions`
+--
+
+INSERT INTO `migration_versions` (`version`, `executed_at`) VALUES
+('20200316175314', '2020-03-16 17:54:10');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+DROP TABLE IF EXISTS `produit`;
+CREATE TABLE IF NOT EXISTS `produit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prix` decimal(10,0) NOT NULL,
+  `date_creation` datetime NOT NULL,
+  `categorie_id` int(11) NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cru` tinyint(1) NOT NULL,
+  `cuit` tinyint(1) NOT NULL,
+  `bio` tinyint(1) NOT NULL,
+  `debut_disponibilite` date DEFAULT NULL,
+  `fin_disponibilite` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_29A5EC27BCF5E72D` (`categorie_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=134 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `produit`
+--
+
+INSERT INTO `produit` (`id`, `libelle`, `prix`, `date_creation`, `categorie_id`, `description`, `cru`, `cuit`, `bio`, `debut_disponibilite`, `fin_disponibilite`) VALUES
+(129, 'mirabelle', '3', '2020-03-30 00:00:00', 77, NULL, 0, 0, 0, NULL, NULL),
+(130, 'pomme', '2', '2020-03-30 00:00:00', 77, NULL, 0, 0, 0, NULL, NULL),
+(131, 'poire', '3', '2020-03-30 00:00:00', 77, NULL, 0, 0, 0, NULL, NULL),
+(132, 'cerise', '3', '2020-03-30 00:00:00', 77, NULL, 0, 0, 0, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `race`
+--
+
+DROP TABLE IF EXISTS `race`;
+CREATE TABLE IF NOT EXISTS `race` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `intitule` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `race`
+--
+
+INSERT INTO `race` (`id`, `intitule`) VALUES
+(2, 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recette`
+--
+
+DROP TABLE IF EXISTS `recette`;
+CREATE TABLE IF NOT EXISTS `recette` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recette_produit`
+--
+
+DROP TABLE IF EXISTS `recette_produit`;
+CREATE TABLE IF NOT EXISTS `recette_produit` (
+  `recette_id` int(11) NOT NULL,
+  `produit_id` int(11) NOT NULL,
+  PRIMARY KEY (`recette_id`,`produit_id`),
+  KEY `IDX_EDDD365D89312FE9` (`recette_id`),
+  KEY `IDX_EDDD365DF347EFB` (`produit_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD CONSTRAINT `FK_29A5EC27BCF5E72D` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`);
+
+--
+-- Contraintes pour la table `recette_produit`
+--
+ALTER TABLE `recette_produit`
+  ADD CONSTRAINT `FK_EDDD365D89312FE9` FOREIGN KEY (`recette_id`) REFERENCES `recette` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_EDDD365DF347EFB` FOREIGN KEY (`produit_id`) REFERENCES `produit` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
